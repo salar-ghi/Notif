8120e3cdaf71f13760516713036fc281169c1a44
Salar-ghi <salar.ghi1993@gmail.com>
2024-05-18 07:51:28 +0330

    change assemblyfiles

--- src/external/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ src/external/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17caadf7bf611f0ad76425ade0e09c7fa47088c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d40a9fd505fcb032df6e69073b6aeaebef9560")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
